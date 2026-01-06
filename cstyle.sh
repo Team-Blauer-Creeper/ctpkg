f7ef7e5742e0c3742d3423e8e1b564f6e789c134
Team-Blauer-Creeper <der.anton.2012@gmail.com>
2026-01-06 18:18:18 +0100

    Aktualisieren von cstyle.sh

--- cstyle.sh
+++ cstyle.sh
@@ -1,2 +1,12 @@
-echo "PROMPT='%F{green}┌──(%F{red}root㉿exploitOS%F{green})-[%F{cyan}%~%F{green}]
-└─$ %f'" > zshrc
+echo "clear
+figlet -f slant EXPLOITOS
+echo "Kernel 6.6.6 | Secure Mode | Encrypted"
+echo "Initializing exploit modules..."
+sleep 1
+echo "Loading payloads..."
+sleep 1
+echo "System ready."
+neofetch
+
+PROMPT='%F{green}┌──(%F{red}root㉿exploitOS%F{green})-[%F{cyan}%~%F{green}]
+└─$ %f'" > .zshrc
