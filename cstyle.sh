7a74dafff7e0bb167c21041fe29861043d8179f9
Team-Blauer-Creeper <der.anton.2012@gmail.com>
2026-01-06 18:07:27 +0100

    Aktualisieren von cstyle.sh

--- cstyle.sh
+++ cstyle.sh
@@ -1,2 +1,2 @@
-echo PROMPT='%F{green}┌──(%F{red}root㉿exploitOS%F{green})-[%F{cyan}%~%F{green}]
-└─$ %f' > zshrc
+echo "PROMPT='%F{green}┌──(%F{red}root㉿exploitOS%F{green})-[%F{cyan}%~%F{green}]
+└─$ %f'" > zshrc
